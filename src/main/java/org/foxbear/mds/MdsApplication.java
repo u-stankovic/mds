f37f39c2e2dbfa4b03f1553345b77879a5bed7b5
Uroš Stanković <93039802+u-stankovic@users.noreply.github.com>
2026-05-21 14:34:42 +0200

    Initial commit

---
+++ src/main/java/org/foxbear/mds/MdsApplication.java
@@ -0,0 +1,13 @@
+package org.foxbear.mds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/foxbear/mds/MdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foxbear.mds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
