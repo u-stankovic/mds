9d20e5e0b05222998e1316826fed91e6c0ffb951
Uroš Stanković <93039802+u-stankovic@users.noreply.github.com>
2026-05-21 19:36:01 +0200

    Add logs for createOrder function

--- src/main/java/org/foxbear/mds/orderapi/controller/OrderController.java
+++ src/main/java/org/foxbear/mds/orderapi/controller/OrderController.java
@@ -1,16 +1,15 @@
 package org.foxbear.mds.orderapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.foxbear.mds.model.OrderEvent;
 import org.foxbear.mds.orderapi.service.OrderProducerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
+@Slf4j
 public class OrderController {
 
     private final OrderProducerService producerService;
@@ -22,7 +21,13 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<Void> createOrder(@RequestBody OrderEvent event) {
 
-        if (event.orderId() == null || event.itemId() == null || event.quantity() <= 0) {
+        if (event.orderId() == null || event.itemId() == null) {
+            log.warn("Nevalidan zahtev: orderId i itemId ne smeju biti null.");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (event.quantity() <= 0) {
+            log.warn("Nevalidna količina za porudžbinu {}: prosleđena vrednost je {}.", event.orderId(), event.quantity());
             return ResponseEntity.badRequest().build();
         }
 
