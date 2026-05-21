fd33c6de4f2e0faf1d7baaa021c4b96010c9b36e
Uroš Stanković <93039802+u-stankovic@users.noreply.github.com>
2026-05-21 19:45:35 +0200

    Add dlt handler

--- src/main/java/org/foxbear/mds/inventory/service/InventoryConsumerService.java
+++ src/main/java/org/foxbear/mds/inventory/service/InventoryConsumerService.java
@@ -2,7 +2,11 @@ package org.foxbear.mds.inventory.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.foxbear.mds.model.OrderEvent;
+import org.springframework.kafka.annotation.BackOff;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -20,6 +24,10 @@ public class InventoryConsumerService {
         inventoryStock.put("item-2", 150);
     }
 
+    @RetryableTopic(
+            backOff = @BackOff(delay = 2000, multiplier = 2.0),
+            dltStrategy = DltStrategy.FAIL_ON_ERROR
+    )
     @KafkaListener(topics = "order-events", groupId = "inventory-group")
     public void processOrder(OrderEvent event) {
         log.info("Konzumer primio porudžbinu za obradu. ID: {}", event.orderId());
@@ -27,6 +35,11 @@ public class InventoryConsumerService {
         if (processedOrders.containsKey(event.orderId())) {
             log.warn("Idempotency aktiviran: Porudžbina {} je već obrađena.", event.orderId());
             return;
+        }
+
+        if ("timeout-item".equals(event.itemId())) {
+            log.error("Simulacija mrežnog prekida za {}. Pokreće se retry.", event.itemId());
+            throw new RuntimeException("Spoljni servis nedostupan");
         }
 
         Integer currentStock = inventoryStock.getOrDefault(event.itemId(), 0);
@@ -41,5 +54,10 @@ public class InventoryConsumerService {
                     event.orderId(), event.quantity(), currentStock);
             processedOrders.put(event.orderId(), true);
         }
+    }
+
+    @DltHandler
+    public void handleDeadLetter(OrderEvent event) {
+        log.error("Porudžbina {} je trajno neuspešna nakon svih pokušaja i prebačena je u DLQ.", event.orderId());
     }
 }
